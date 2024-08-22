9fd99e92e626a66289cdf7a1ab677a8d3c138426
PRABHJOT SINGH <121215854+prabhjot-singh18@users.noreply.github.com>
2024-08-22 11:34:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+#controls {
+    position: fixed;
+    top: 10px;
+    left: 10px;
+    background-color: rgba(255, 255, 255, 0.8);
+    padding: 10px;
+    border-radius: 5px;
+    z-index: 1;
+}
+
+label, select, button {
+    margin: 5px 0;
+    display: block;
+}
+
+button {
+    padding: 10px 20px;
+    background-color: #007BFF;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #0056b3;
+}
